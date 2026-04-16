1f20d23f19fb795c4564c8e61764c0b500316526
ahenoai-del <ahenoai@gmail.com>
2026-04-16 21:51:42 +0300

    Delete bot directory

--- bot/handlers/__init__.py
+++
@@ -1,8 +0,0 @@
-from .start import router as start_router
-from .tasks import router as tasks_router
-from .planning import router as planning_router
-from .webapp_handler import router as webapp_router
-from .admin import router as admin_router
-from .payments import router as payments_router
-
-__all__ = ["start_router", "tasks_router", "planning_router", "webapp_router", "admin_router", "payments_router"]
